508420a1b9d88088ce8fda11d9b5c7c011731da5
olek sysio <oleksysio@oleks-MacBook-Air.local>
2023-02-25 20:26:51 +0100

    Addition routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Login />} >
+          <Route path="sign" element={<Sign />} />
+        </Route>        
+      </Routes>
     </div>
   );
 }
